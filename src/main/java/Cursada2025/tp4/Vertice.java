818c1c8c66a70f5c125651a3e01df3281f7a3d50
SrtaLiv <olitutos712@gmail.com>
2025-04-25 17:41:47 -0300

    notas

--- src/main/java/Cursada2025/tp4/Vertice.java
+++ src/main/java/Cursada2025/tp4/Vertice.java
@@ -3,6 +3,10 @@ package Cursada2025.tp4;
 import java.util.ArrayList;
 
 public class Vertice<T>{
+
+    //if (!vertices.contains(vertieId)) o(n)
+    // no es buena opcion tener lista de arcos pq si me piden de Xvertice tendria q recorrer TODOS los arcos
+
     private int id;
     private ArrayList<Integer> adyacentes;
     private T etiqueta;
